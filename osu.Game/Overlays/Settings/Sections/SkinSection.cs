a018541cdb4ae6e6f9689c383c86e6f83e3fca22
XDi00 <xd6548@gmail.com>
2023-11-20 19:21:56 +0800

    z

--- osu.Game/Overlays/Settings/Sections/SkinSection.cs
+++ osu.Game/Overlays/Settings/Sections/SkinSection.cs
@@ -104,6 +104,7 @@ namespace osu.Game.Overlays.Settings.Sections
             dropdownItems.Clear();
 
             dropdownItems.Add(sender.Single(s => s.ID == SkinInfo.ARGON_SKIN).ToLive(realm));
+            dropdownItems.Add(sender.Single(s => s.ID == SkinInfo.ARGON_MAX_SKIN).ToLive(realm));
             dropdownItems.Add(sender.Single(s => s.ID == SkinInfo.ARGON_PRO_SKIN).ToLive(realm));
             dropdownItems.Add(sender.Single(s => s.ID == SkinInfo.TRIANGLES_SKIN).ToLive(realm));
             dropdownItems.Add(sender.Single(s => s.ID == SkinInfo.CLASSIC_SKIN).ToLive(realm));
---
+++ osu.Game/Skinning/ArgonMaxSkin.cs
@@ -0,0 +1,48 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using JetBrains.Annotations;
+using osu.Framework.Audio.Sample;
+using osu.Game.Audio;
+using osu.Game.Extensions;
+using osu.Game.IO;
+
+namespace osu.Game.Skinning
+{
+    public class ArgonMaxSkin : ArgonSkin
+    {
+        public new static SkinInfo CreateInfo() => new SkinInfo
+        {
+            ID = Skinning.SkinInfo.ARGON_MAX_SKIN,
+            Name = "osu! \"argon\" max (2022)",
+            Creator = "team osu!",
+            Protected = true,
+            InstantiationInfo = typeof(ArgonMaxSkin).GetInvariantInstantiationInfo()
+        };
+
+        public override ISample? GetSample(ISampleInfo sampleInfo)
+        {
+            foreach (string lookup in sampleInfo.LookupNames)
+            {
+                string remappedLookup = lookup.Replace(@"Gameplay/", @"Gameplay/ArgonPro/");
+
+                var sample = Samples?.Get(remappedLookup) ?? Resources.AudioManager?.Samples.Get(remappedLookup);
+                if (sample != null)
+                    return sample;
+            }
+
+            return null;
+        }
+
+        public ArgonMaxSkin(IStorageResourceProvider resources)
+            : this(CreateInfo(), resources)
+        {
+        }
+
+        [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+        public ArgonMaxSkin(SkinInfo skin, IStorageResourceProvider resources)
+            : base(skin, resources)
+        {
+        }
+    }
+}
